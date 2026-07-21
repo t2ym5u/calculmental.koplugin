1c754761608d09efd134a6c5b124e5adb95e7c08
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-21 13:39:29 +0200

    chore: bump version to 1.1.3

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "calculmental",
     fullname    = _("Calcul Mental"),
     description = _("Entraînez vos réflexes arithmétiques."),
-    version     = "1.1.2",
+    version     = "1.1.3",
 }
