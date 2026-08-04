114275efb4103fcf537b2a6e918b6ce94e677093
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-04 12:51:01 +0200

    chore: bump version to 1.1.12

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Calcul Mental"),
     description = _("Entraînez vos réflexes arithmétiques."),
-    version     = "1.1.11",
+    version     = "1.1.12",
 }
