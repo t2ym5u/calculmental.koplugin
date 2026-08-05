8671f537314ebf6f35b90b9eb4852bde013f1e0f
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:59:25 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,6 +1,6 @@
 return {
-    ["Mental Math"]                      = { fr = "Calcul Mental" },
-    ["Train your arithmetic reflexes."]  = { fr = "Entraînez vos réflexes arithmétiques." },
-    ["Choose an answer!"]                = { fr = "Choisissez une réponse !" },
-    ["%d/%d (%d%%)  Streak: %d%s"]       = { fr = "%d/%d (%d%%)  Série: %d%s" },
+    ["Mental Math"]                      = { fr = "Calcul Mental", es = "Cálculo Mental", de = "Kopfrechnen" },
+    ["Train your arithmetic reflexes."]  = { fr = "Entraînez vos réflexes arithmétiques.", es = "Entrena tus reflejos aritméticos.", de = "Trainiere deine Rechenreflexe." },
+    ["Choose an answer!"]                = { fr = "Choisissez une réponse !", es = "¡Elige una respuesta!", de = "Wähle eine Antwort!" },
+    ["%d/%d (%d%%)  Streak: %d%s"]       = { fr = "%d/%d (%d%%)  Série: %d%s", es = "%d/%d (%d%%)  Racha: %d%s", de = "%d/%d (%d%%)  Serie: %d%s" },
 }
